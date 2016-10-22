ef8495c38d712df8f2f46401fb0bd819b486f33e
Tracy Homer <thomer@mac.com>
2016-10-22 15:39:08 +0000

    Tracy and Wolfie's project

--- create_box/main.py
+++ create_box/main.py
@@ -2,8 +2,36 @@
 
 
 def create_box(height, width, character):
-    pass
+    if height < 1:
+        if width < 1:
+            return 'Box not big enough.'
+    box = []
+    for i in range(0,height):
+        box.append(character * width)
+    return '\n'.join(box) + '\n'
+    
+    
+print(create_box(3,4,'o'))
+
+
+def create_empty_box(height,width,character):
+    if height < 3:
+        if width < 3:
+            return 'Box not big enough'
+    box = []
+    for i in range(0,height):
+        if i == 0:
+            box.append(character * width)
+        elif 0<i<height:
+            box.append(character + ' '* int(width-2) + character)
+        if i == height-1:
+            box.append(character * width)
+    return '\n'.join(box) + '\n'
+
+print(create_empty_box(3,4,'o'))
 
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
+    
+    
